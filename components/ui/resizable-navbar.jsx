dc1285052e2d2776b01a9885ab66d9b4c0dd0c08
sombit-k <sombitkarmakar018@gmail.com>
2025-07-27 21:32:15 +0530

    feat: Replace anchor tag with Link component in NavbarLogo for improved routing

--- components/ui/resizable-navbar.jsx
+++ components/ui/resizable-navbar.jsx
@@ -193,8 +193,8 @@ export const MobileNavToggle = ({
 
 export const NavbarLogo = () => {
   return (
-    (<a
-      href="#"
+    (<Link
+      href="/"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black">
       <img
         src="https://assets.aceternity.com/logo-dark.png"
@@ -202,7 +202,7 @@ export const NavbarLogo = () => {
         width={30}
         height={30} />
       <span className="font-medium text-black dark:text-white">Startup</span>
-    </a>)
+    </Link>)
   );
 };
 
